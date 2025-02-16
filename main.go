843aa1ae35cb5e539ca173d2b64affce3b27a194
Gaponenko <Egor.Gapo@yandex.ru>
2025-02-16 14:08:15 +0400

    Сообщение о коммите

--- main.go
+++ main.go
@@ -5,6 +5,16 @@ import "fmt"
 func main() {
 	var a string = "ver4"
 	fmt.Print(a)
-
+	fmt.Print(a)
+	fmt.Print(a)
+	fmt.Print(a)
+	fmt.Print(a)
+	fmt.Print(a)
+	fmt.Print(a)
+	fmt.Print(a)
+	fmt.Print(a)
+	fmt.Print(a)
+	fmt.Print(a)
+	fmt.Print(a)
 	println(a)
 }
