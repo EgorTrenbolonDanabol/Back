a2fa22c45aaeb87b2d1cd30c175be5fca95ea85b
Gaponenko <Egor.Gapo@yandex.ru>
2025-02-16 14:16:04 +0400

    "ver5"

--- main.go
+++ main.go
@@ -1,20 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
 	var a string = "ver4"
-	fmt.Print(a)
-	fmt.Print(a)
-	fmt.Print(a)
-	fmt.Print(a)
-	fmt.Print(a)
-	fmt.Print(a)
-	fmt.Print(a)
-	fmt.Print(a)
-	fmt.Print(a)
-	fmt.Print(a)
-	fmt.Print(a)
-	fmt.Print(a)
 	println(a)
 }
